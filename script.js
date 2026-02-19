e6d62b90c82fc1c1c9cfa32d47218b2aacbd77e4
barbara <barbmelo20gmail.com>
2026-02-19 05:38:38 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 //menu hamburguer
 const menuToggle = document.getElementById('menu-toggle');
-const nav = document.querySelector('nav');
+const nav = document.querySelector('.nav');
 menuToggle.addEventListener('click',()=>{
-    nav.classList.toggle('active');
+    nav.classList.toggle('nav--active');
 });
+//toggle tema
 const toggle = document.getElementById('theme-toggle');
 //verifica se já existe tema salvo
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'dark') {
-    document.body.classList.add('dark-mode');
+    document.body.classList.add('body--dark');
     toggle.textContent = '☼';
 } else {
     toggle.textContent = '☾';
 }
 //altera o tema
 toggle.addEventListener('click',()=>{
-    document.body.classList.toggle('dark-mode');
-    if (document.body.classList.contains('dark-mode')) {
+    document.body.classList.toggle('body--dark');
+    if (document.body.classList.contains('body--dark')) {
         localStorage.setItem('theme', 'dark');
         toggle.textContent = '☼';
     } else {
